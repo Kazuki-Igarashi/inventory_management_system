118464efbd642bee3bc965f0f81c23e401b0160b
Kazuki-Igarashi <vki.0504@gmail.com>
2023-11-05 15:22:41 +0000

    論理削除、退会機能の実装

--- app/controllers/public/contractors_controller.rb
+++ app/controllers/public/contractors_controller.rb
@@ -1,13 +1,47 @@
 class Public::ContractorsController < ApplicationController
-  def show
+  before_action :authenticate_customer!
+
+  def show #顧客情報表示
+    @customer = current_customer
   end
 
-  def edit
+  def edit #顧客情報編集
+    @customer = current_customer
   end
 
-  def quit
+  def update #顧客情報アプデ
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to mypage_contractors_path
+    # if @customer.update(customer_params)
+    #   flash[:update] = "You have updated user info successfully."
+    #   redirect_to mypage_public_customers_path
+    # else
+    #   render 'edit'
+    # end
   end
 
-  def out
+  def quit #退会確認画面
+    @customer = current_customer
   end
+
+  def withdraw #退会処理
+    @customer = current_customer
+    # is_memberカラムをtrueに変更することで削除フラグを立てる
+    @customer.update(is_menber: true)
+    if @customer.save
+      reset_session
+      flash[:notice] = "退会処理を実行しました"
+      redirect_to root_path
+    end
+  end
+
+
+  private
+    def customer_params
+      params.require(:customer).permit(:name, :name_kana,
+                                       :email, :post_code, :address, :telephone_number, :company_name, :company_name_kana )
+    end
+
 end
+
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,6 +3,8 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+   before_action :customer_state, only: [:create]
+  
   # GET /resource/sign_in
   # def new
   #   super
@@ -19,6 +21,23 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   # protected
+
+  protected
+  # 退会しているか判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+  @customer = Customer.find_by(email: params[:customer][:email])
+  ## アカウントを取得できなかった場合、このメソッドを終了する
+  return if !@customer
+  ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+      if @customer.is_menber == true #customerの入会ステータスを確認し、退会の場合ログインできないようにする
+        redirect_to new_customer_registration_path
+      else
+        return
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
